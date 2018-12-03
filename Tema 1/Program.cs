38f0a632a6e42e74aa4b776ce13854be76142c47
Andrei Vasiloi <andrei_vasiloi@yahoo.com>
2018-12-03 18:06:19 +0200

    Add GUESS THE NUMBER game

--- Tema 1/Program.cs
+++ Tema 1/Program.cs
@@ -10,6 +10,60 @@ namespace Tema_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Do you want to play a game?");
+            string answer = Console.ReadLine();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("Ok, let`s play GUESS THE NUMBER.");
+            }
+            else
+            {
+                Console.WriteLine("Ok, Have a nice day!");
+                Console.ReadLine();
+                return;
+            }
+
+
+            Random rng = new Random();
+            int randomNumber = rng.Next(1, 100);
+
+            int userNumber = randomNumber;
+
+            Console.WriteLine("Please enter a number between 1 and 100 and press enter.");
+
+            do
+            {
+
+                string str = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine("You must enter a value between 1 and 100.");
+                    continue;
+                }
+
+                userNumber = int.Parse(str);
+                if (userNumber < randomNumber)
+                {
+                    Console.WriteLine("The number is shorter!");
+                }
+                else if (userNumber == randomNumber)
+                {
+                    Console.WriteLine("The number is correct!");
+                    Console.WriteLine("You won the game!");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The number is greater!");
+                }
+            } while (userNumber >= 0);
         }
     }
 }
